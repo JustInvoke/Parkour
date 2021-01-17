3064ec34e8000d97952eaa87ce751b3bd936c714
MitchDesktop <mitchellhogle@yahoo.com>
2021-01-17 14:39:05 -0800

    created GlobalGameManager and Title Screen scene

---
+++ Assets/Scripts/Misc/GlobalGameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalGameManager : MonoBehaviour
+{
+    //Singleton Reference
+    public static GlobalGameManager Instance { get { return _instance; } }
+    private static GlobalGameManager _instance;
+    private void Awake() {
+        //Set this object not to destroy on loading new scenes
+        DontDestroyOnLoad(gameObject);
+
+        //Setup of Monobehaviour Singleton
+        if (_instance != this && _instance != null) {
+            Destroy(_instance.gameObject);
+        }
+        _instance = this;
+    }
+}

4ce59d05df04b8729dfe1c238ae57975ac373533
Christian Benavidez <christianbenavidez57@gmail.com>
2021-02-06 18:03:25 -0800

    Spawn difficult chunks after a certain interval
    Hard chunks will spawn after a certain interval determined by
    hardChunkFreq. There is a separate array for these hard chunks than
    the regular level chunks. The frequency of spawning increases over
    time so thats what increases difficulty over time.
    
    The speed of the character prefab was also increased just to make
    the game a bit easier to test out.
    
    I should note that the game play is a bit slow and more testing will
    need to be done to see what values should be tweaked in the prefab.
    But that is a simple variable change in the Unity UI.

--- Assets/Scripts/Level/LevelManager.cs
+++ Assets/Scripts/Level/LevelManager.cs
@@ -8,6 +8,11 @@ public class LevelManager : MonoBehaviour
     public GameObject firstChunk; // First chunk to spawn, useful so that the level starts smoothly
     public int firstChunkCount = 1; // Number of times to repeat the starting chunk
     public GameObject[] levelChunks; // Array of possible chunks to spawn
+    public GameObject[] hardChunks; // Array of hard chunks to spawn
+    private int totalChunksSpawned = 0; // Total number of chunks ever spawned
+    public int hardChunkFreq = 10; // How many chunk spawns until next hard chunk spawn?
+    public int minHardChunkFreq = 4; // Minimum amount of chunks that need to spawn until next hard chunk
+    public int chunkMilestone = 100; // How many chunks need to spawn until next hardChunkFreq Decrement
     private Queue<LevelChunk> spawnedChunks = new Queue<LevelChunk>(); // Queue of currently spawned chunks
     private LevelChunk lastSpawnedChunk; // Reference to the most recently spawned chunk
     public Camera mainCam; // Reference to the main camera in the scene
@@ -48,9 +53,16 @@ public class LevelManager : MonoBehaviour
         }
     }
 
-    // Spawn a random chunk from the array of chunks
+    // Spawn a random chunk from one array of chunks
     void SpawnChunk() {
-        SpawnChunk(levelChunks[Random.Range(0, levelChunks.Length)]);
+        // Spawn from hard chunks if enough easy chunks have spawned
+        if (totalChunksSpawned != 0 && totalChunksSpawned % hardChunkFreq == 0)
+        {
+            SpawnChunk(hardChunks[Random.Range(0, hardChunks.Length)]);
+        }
+        else {
+            SpawnChunk(levelChunks[Random.Range(0, levelChunks.Length)]);
+        }
     }
 
     // Spawn a specific chunk
@@ -67,6 +79,14 @@ public class LevelManager : MonoBehaviour
         // Spawn the chunks and add it to the queue
         lastSpawnedChunk = Instantiate(chunk, new Vector3(spawnPos.x, spawnPos.y, 0.0f), Quaternion.identity).GetComponent<LevelChunk>();
         spawnedChunks.Enqueue(lastSpawnedChunk);
+        // Increment the number of total chunks ever spawned
+        totalChunksSpawned++;
+        // Update the frequency of hard chunk spawns if the chunk milestone is surpassed
+        if (totalChunksSpawned != 0 && totalChunksSpawned % chunkMilestone == 0) {
+            if (hardChunkFreq > minHardChunkFreq) {
+                hardChunkFreq--;
+            }
+        }
     }
 
     // Reload the current level

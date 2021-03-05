21924538988349a1ffa552d9bfc956d97a84687a
Justin Couch <justin@justinvoke.com>
2021-03-04 16:55:08 -0800

    Disabled the shield item and added item spawners to other level chunks

--- Assets/Scripts/Items/ItemSpawner.cs
+++ Assets/Scripts/Items/ItemSpawner.cs
@@ -8,7 +8,7 @@ public class ItemSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (Random.Range(0, 3) == 0) {  //25% chance to spawn item
+        if (Random.Range(0, 4) == 0) {  //25% chance to spawn item
             int i = Random.Range(0, items.Count);
             GameObject item = Instantiate(items[i].gameObject);
             item.transform.position = transform.position;
